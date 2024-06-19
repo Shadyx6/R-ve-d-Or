73b1923bfc541512fe5f8f8f6ba87382458d1173
Shadyx6 <shadkhanxox@gmail.com>
2024-06-20 00:18:05 +0530

    improved Login UI and added searchBar on individual product page for mobiles

--- public/javascripts/header.js
+++ public/javascripts/header.js
@@ -9,9 +9,10 @@ function handleClick() {
     collapseMenu.style.display = 'block';
   }
 }
+if (toggleOpen && toggleClose) {
 toggleOpen.addEventListener('click', handleClick);
 toggleClose.addEventListener('click', handleClick);
-
+}
 function isActive(url) {
   if(req.path === url) {
     active
@@ -19,3 +20,16 @@ function isActive(url) {
     white
   }
 }
+const searchButton = document.getElementById("searchButton");
+const searchBar = document.getElementById("searchBar");
+const cartIcon = document.querySelector(".cart-header");
+const orderIcon = document.querySelector(".order-header");
+
+searchButton.addEventListener("click", async () => {
+  searchBar.classList.toggle("hidden");
+  searchButton.classList.add("hidden");
+cartIcon.classList.add("hidden");
+orderIcon.classList.add('hidden')
+
+});
+
